227c5a4dab15e33d508b6a15960af66220939d54
jakubjurczak <jakub.jurczak@wayfdigital.com>
2023-03-02 23:14:29 +0100

    fix: locale paths

--- pages/paintings/collection/[collectionId].tsx
+++ pages/paintings/collection/[collectionId].tsx
@@ -11,10 +11,10 @@ import { useState } from 'react';
 import { ImagePreview } from '@/components/image-preview';
 
 interface DrawingsPageProps {
-  drawings: Artwork[];
+  paintings: Artwork[];
 }
 
-export default function DrawingsPage({ drawings }: DrawingsPageProps) {
+export default function DrawingsPage({ paintings }: DrawingsPageProps) {
   const [image, setImage] = useState<Artwork>();
   const [previewVisible, setPreviewVisible] = useState<boolean>(false);
   const [imageLoading, setImageLoading] = useState<boolean>(false);
@@ -29,25 +29,25 @@ export default function DrawingsPage({ drawings }: DrawingsPageProps) {
 
   const handleNextClick = () => {
     setImageLoading(true);
-    const index = drawings.findIndex(({ id }) => id === image?.id);
+    const index = paintings.findIndex(({ id }) => id === image?.id);
     const nextIndex = index + 1;
-    const nextImage = drawings[nextIndex] ?? drawings[0];
+    const nextImage = paintings[nextIndex] ?? paintings[0];
     setImage(nextImage);
   };
 
   const handlePrevClick = () => {
     setImageLoading(true);
-    const index = drawings.findIndex(({ id }) => id === image?.id);
+    const index = paintings.findIndex(({ id }) => id === image?.id);
     const prevIndex = index - 1;
-    const prevImage = drawings[prevIndex] ?? drawings[drawings.length - 1];
+    const prevImage = paintings[prevIndex] ?? paintings[paintings.length - 1];
     setImage(prevImage);
   };
 
   return (
     <>
       <ul className="grid  gap-4 justify-center p-[initial] grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
-        {drawings.map((drawing) => (
-          <ImageTile key={drawing.id} image={drawing} onClick={handleShowPreview} />
+        {paintings.map((painting) => (
+          <ImageTile key={painting.id} image={painting} onClick={handleShowPreview} />
         ))}
       </ul>
       <ImagePreview
@@ -68,12 +68,11 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
   const { data } = await authAxios().get(
     `${API_URL}/painting-collections/${collectionId}?populate[paintings][populate][0]=media_file`,
   );
-
-  const drawings = mapPaintingResponseToPainting(data.data.attributes.paintings);
+  const paintings = mapPaintingResponseToPainting(data?.data?.attributes?.paintings);
 
   return {
     props: {
-      drawings,
+      paintings,
       ...(await serverSideTranslations(extractLocale(ctx), ['common'])),
     },
   };
